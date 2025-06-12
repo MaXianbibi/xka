5233ac786227c6e4a5e5bd51e6d510ab86bbdc30
Justin Morneau <jmorneau25@gmail.com>
2025-06-12 18:21:21 -0400

    Add Dashboard component and integrate into layout; update dependencies

---
+++ app/Dashboard/Page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className="bg-gradient-to-b from-zinc-950 to-zinc-900 h-svh w-[12vw] min-w-[180px] max-w-[240px] border-r border-zinc-800 shadow-lg" />
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Dashboard from "@/app/Dashboard/Page"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-row h-screen w-screen">
+          <Dashboard />
+          {children}
+        </div>
       </body>
     </html>
   );
